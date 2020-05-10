b80f679e2bb350e3c6b99957dcb307182db3cb0e
Seweryn Nyckowski <sewern@poczta.onet.pl>
2020-05-10 12:59:47 +0200

    test commit

---
+++ Pharmacy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pharmacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
